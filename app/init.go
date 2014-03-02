a5cd7f9971d745632c7885b24dfb95c96d3265b4
vassilux <v.gontcharov@gmail.com>
2014-03-02 19:57:57 +0100

    Initial commit

---
+++ app/init.go
@@ -0,0 +1,38 @@
+package app
+
+import (
+	//"github.com/cbonello/revel-csrf"
+	"github.com/robfig/revel"
+	"log"
+)
+
+var CSRFFilter = func(c *revel.Controller, fc []revel.Filter) {
+	log.Printf("[init] CSRFFilter cross origin allowed")
+	c.Response.Out.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Response.Out.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	c.Response.Out.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
+	c.Response.Out.Header().Set("Access-Control-Allow-Credentials", "true")
+	//
+	fc[0](c, fc[1:])
+}
+
+func init() {
+	// Filters is the default set of global filters.
+	revel.Filters = []revel.Filter{
+		revel.PanicFilter,             // Recover from panics and display an error page instead.
+		revel.RouterFilter,            // Use the routing table to select the right Action
+		revel.FilterConfiguringFilter, // A hook for adding or removing per-Action filters.
+		revel.ParamsFilter,            // Parse parameters into Controller.Params.
+		revel.SessionFilter,           // Restore and write the session cookie.
+		revel.FlashFilter,             // Restore and write the flash cookie.
+		CSRFFilter,                    // CSRF prevention.
+		revel.ValidationFilter,        // Restore kept validation errors and save new ones from cookie.
+		revel.I18nFilter,              // Resolve the requested language
+		revel.InterceptorFilter,       // Run interceptors around the action.
+		revel.CompressFilter,          // Compress the result.
+		revel.ActionInvoker,           // Invoke the action.
+	}
+
+	/*csrf.ExemptedFullPath("/loginget")
+	csrf.ExemptedFullPath("/login")*/
+}
